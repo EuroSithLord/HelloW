000b3ca1746a9a7d0728320da12fe2baa1e76a8d
EuroSithLord <liviu.ganea7@gmail.com>
2021-07-24 17:47:39 +0300

    Public members documentation

--- HelloW/Program.cs
+++ HelloW/Program.cs
@@ -2,14 +2,19 @@
 
 namespace HelloW
 {
-    class Program
+    /// <summary>
+    ///     Application starting point.
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             SendToConsole("Hello Epa ... I mean, Hello World!!!");
         }
-
+        /// <summary>
+        ///     Takes <paramref name="message"/> and outputs it to console.
+        /// </summary>
         public static void SendToConsole(String message)
         {
             Console.WriteLine(message);
