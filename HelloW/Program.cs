9743de4a6fa577ec6d012b5b1c52b37154b119ef
EuroSithLord <liviu.ganea7@gmail.com>
2021-07-24 18:06:53 +0300

    Removed unnecessary code.

--- HelloW/Program.cs
+++ HelloW/Program.cs
@@ -10,14 +10,6 @@ namespace HelloW
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            SendToConsole("Hello Epa ... I mean, Hello World!!!");
-        }
-        /// <summary>
-        ///     Takes <paramref name="message"/> and outputs it to console.
-        /// </summary>
-        public static void SendToConsole(String message)
-        {
-            Console.WriteLine(message);
         }
     }
 }
