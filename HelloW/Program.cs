d72bc19655872180a58ce9964302bae6e4dfaa28
EuroSithLord <liviu.ganea7@gmail.com>
2021-07-24 17:39:25 +0300

    First commit

---
+++ HelloW/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SendToConsole("Hello Epa ... I mean, Hello World!!!");
+        }
+
+        public static void SendToConsole(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
